d8bd2db577004072c432ca2625af99375c646f49
eunji <enji4882@gmail.com>
2025-11-04 22:01:36 +0900

    Add files via upload

---
+++ analyzer.h
@@ -0,0 +1,6 @@
+#ifndef ANALYZER_H
+#define ANALYZER_H
+#include <stddef.h>
+
+int get_score(const char* operation, const char* buf, size_t size);
+#endif
---
+++ entropy.h
@@ -0,0 +1,5 @@
+#ifndef ENTROPY_H
+#define ENTROPY_H
+#include <stddef.h>
+double calculate_entropy(const char*buffer, size_t size);
+#endif
